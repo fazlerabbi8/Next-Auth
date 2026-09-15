fff259d33cf97c026782808b01081acd1d012521
fazlerabbi13 <mohammad0132794@gmail.com>
2026-09-15 11:58:37 +0600

    user finding from server and showing it client on side.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,14 @@ import { IoShieldCheckmarkSharp } from "react-icons/io5";
 import { SiMongodb } from "react-icons/si";
 import Link from "next/link";
 import LoginButton from "@/components/LoginButton";
-export default function Home() {
+import UserCard from "@/components/UserCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <div className="min-h-screen relative flex flex-col justify-center items-center gap-5 ">
+      <UserCard></UserCard>
       <div className=" flex gap-5 space-x-4 items-center">
         <FaReact
           size={40}
@@ -25,6 +30,10 @@ export default function Home() {
           Register
         </Link>
       </div>
+       <div>
+            <p className="border p-5">user from server</p>
+            <p className="border p-5">{JSON.stringify(session)}</p>
+          </div>
     </div>
   );
 }
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+
+const UserCard = () => {
+  const { data: session, status } = useSession();
+
+  console.log(session)
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (!session) {
+    return <p>You are not signed in.</p>;
+  }
+
+  return (
+    <div>
+      <p className="border p-5">Signed in as {session.user.name}</p>
+      <p className="border p-5">{JSON.stringify(session)}</p>
+
+      <button className="btn" onClick={() => signOut()}>
+        Sign out
+      </button>
+    </div>
+  );
+};
+
+export default UserCard;
